9d5df3d68c1908ce8108beda7c0550c987d9f297
veryspry <ehlinger.matt@gmail.com>
2019-03-29 14:59:30 -0500

    Add form validation pattern and password masking

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import requestConstructor from "./request";
 import logout from "./logout";
 import getCurrentUser from "./current-user";
+import { validateEmail } from "./validate-email";
 
-export { requestConstructor, logout, getCurrentUser };
+export { requestConstructor, logout, getCurrentUser, validateEmail };
---
+++ src/utils/validate-email.js
@@ -0,0 +1,3 @@
+const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+export const validateEmail = email => emailRegex.test(email);
